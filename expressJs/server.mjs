82ec7e3444e64672059e470557d9a4fd0e752962
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-04-08 01:01:47 +0500

    [Add: GET request method]

--- expressJs/server.mjs
+++ expressJs/server.mjs
@@ -1,1 +1,19 @@
-console.log("Hello world from basic server.")
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// GET method
+
+app.get("/", (req, res) => {
+  res.send("Hello Word");
+});
+
+app.get("/", (req, res) => {
+    res.send("Thanks for visiting this page.");
+  });
+  
+
+app.listen(PORT, () => {
+  console.log(`This server is listening on PORT ${PORT}`);
+});
